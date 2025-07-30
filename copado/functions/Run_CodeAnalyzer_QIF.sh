1251c167a416e3005568050758f9797f98e0312d
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2025-07-30 15:48:18 -0400

    Populate error message  on failure, improved error handling.

--- copado/functions/Run_CodeAnalyzer_QIF.sh
+++ copado/functions/Run_CodeAnalyzer_QIF.sh
@@ -17,21 +17,23 @@ copado-git-get $BRANCH
 ########### Create delta packages for new, modified or deleted metadata  ############
 copado -p "Generating Diff between the Source and Destination branches..."
 mkdir changed-sources
-sf sgd source delta --to "HEAD" --from "origin/$destinationBranch" --output changed-sources/ --generate-delta --source .
+sf sgd source delta --to "HEAD" --from "origin/$destinationBranch" --output-dir changed-sources/ --generate-delta --source .
 echo "Here's the files that have been changes in this US"
 cat changed-sources/package/package.xml
 
 ################ Run Code Analyzer only on Changed Metadata  ###############
 copado -p "Running Code Analyzer..."
 exitCode=0
-sf code-analyzer run --rule-selector all --workspace ./changed-sources/ --view detail --severity-threshold $severityThreshold --output-file ./output.json || exitCode=$?
+errorMessage=""
+sf code-analyzer run --rule-selector all --workspace ./changed-sources/ --view detail --severity-threshold $severityThreshold --output-file ./output.json > stdout.log 2> stderr.log || exitCode=$?
+errorMessage=$(grep "^Error ([0-9]*):" stderr.log 2>/dev/null || echo "")
 
 ############ Attach Results to the Function results record  ####################
 copado -p "Uploading results..."
 if [ -f "output.json" ]; then
     copado -u output.json
-    copado -p "Writing to result" -e "There was an error running Code Analyzer, please check logs for details."
+    copado -p "Writing to result" -e "${errorMessage}"
 fi
 
-echo "Code Analyzer scan completed. exit code: $exitCode"
+copado -p "Code Analyzer scan completed."
 exit $exitCode
