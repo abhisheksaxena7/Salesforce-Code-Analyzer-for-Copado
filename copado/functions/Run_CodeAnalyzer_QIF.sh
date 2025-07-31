8c808af301a0aa6c49ab451d7338e5f10e004f90
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2025-07-30 22:46:23 -0400

    Read severityThreshold from pipeline system property.

--- copado/functions/Run_CodeAnalyzer_QIF.sh
+++ copado/functions/Run_CodeAnalyzer_QIF.sh
@@ -14,6 +14,13 @@ copado -p "Cloning repo..."
 copado-git-get $destinationBranch
 copado-git-get $BRANCH
 
+########## Read Severity from System Property on the Pipeline record, or default to 2 #############
+if [ -z "${severityThreshold:-}" ] || [ -z "$(echo "$severityThreshold" | tr -d '[:space:]')" ]; then
+    severityThreshold=2
+elif ! [[ "$severityThreshold" =~ ^[1-5]$ ]]; then
+    severityThreshold=2
+fi
+
 ########### Create delta packages for new, modified or deleted metadata  ############
 copado -p "Generating Diff between the Source and Destination branches..."
 mkdir changed-sources
@@ -26,7 +33,7 @@ copado -p "Running Code Analyzer..."
 exitCode=0
 errorMessage=""
 sf code-analyzer run --rule-selector all --workspace ./changed-sources/ --view detail --severity-threshold $severityThreshold --output-file ./output.json > stdout.log 2> stderr.log || exitCode=$?
-errorMessage=$(grep "^Error ([0-9]*):" stderr.log 2>/dev/null || echo "")
+errorMessage=$(grep "^Error ([0-9]*):" stderr.log 2>/dev/null | sed -E 's/^Error \([0-9]*\): //g' 2>/dev/null || echo "")
 
 ############ Attach Results to the Function results record  ####################
 copado -p "Uploading results..."
